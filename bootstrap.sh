cb08d5c13e01ac924b2bcbf8a1160a2d6622eab6
Dan Barr <6922515+danbarr@users.noreply.github.com>
2026-02-17 13:20:06 -0500

    Bump toolhive versions
    Signed-off-by: Dan Barr <6922515+danbarr@users.noreply.github.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,10 +17,10 @@ TEMPO_CHART_VERSION="1.26.2" # renovate: datasource=helm depName=tempo registryU
 PROMETHEUS_CHART_VERSION="28.9.1" # renovate: datasource=helm depName=prometheus registryUrl=https://prometheus-community.github.io/helm-charts
 GRAFANA_CHART_VERSION="11.1.7" # renovate: datasource=helm depName=grafana registryUrl=https://grafana-community.github.io/helm-charts
 CLOUDNATIVE_PG_CHART_VERSION="0.27.1" # renovate: datasource=helm depName=cloudnative-pg registryUrl=https://cloudnative-pg.github.io/charts
-TOOLHIVE_OPERATOR_CRDS_CHART_VERSION="0.9.3" # renovate: datasource=docker depName=ghcr.io/stacklok/toolhive/toolhive-operator-crds
-TOOLHIVE_OPERATOR_CHART_VERSION="0.9.3" # renovate: datasource=docker depName=ghcr.io/stacklok/toolhive/toolhive-operator
+TOOLHIVE_OPERATOR_CRDS_CHART_VERSION="0.9.4" # renovate: datasource=docker depName=ghcr.io/stacklok/toolhive/toolhive-operator-crds
+TOOLHIVE_OPERATOR_CHART_VERSION="0.9.4" # renovate: datasource=docker depName=ghcr.io/stacklok/toolhive/toolhive-operator
 REGISTRY_SERVER_CHART_VERSION="0.6.2" # renovate: datasource=docker depName=ghcr.io/stacklok/toolhive-registry-server
-CLOUD_UI_VERSION="v0.1.0" # renovate: datasource=docker depName=ghcr.io/stacklok/toolhive-cloud-ui
+CLOUD_UI_VERSION="v0.3.0" # renovate: datasource=docker depName=ghcr.io/stacklok/toolhive-cloud-ui
 MCP_OPTIMIZER_CHART_VERSION="0.2.5" # renovate: datasource=docker depName=ghcr.io/stackloklabs/mcp-optimizer/mcp-optimizer
 
 # Source common helper functions
